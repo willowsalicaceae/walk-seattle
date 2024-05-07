922d7d00687a71a6e55c9f069ea4252a60e0c652
willow <95714181+willowsalicaceae@users.noreply.github.com>
2024-05-07 14:54:38 -0700

    Added logo and profile image to appbar

--- src/components/Navigation/AppBar.js
+++ src/components/Navigation/AppBar.js
@@ -1,17 +1,21 @@
 import React from 'react';
-import { AppBar, Toolbar, Typography, IconButton } from '@mui/material';
-import { Menu } from '@mui/icons-material';
+import { AppBar, Toolbar, Avatar } from '@mui/material';
+import { ReactComponent as Logo } from '../../img/logo.svg';
+import { Link } from 'react-router-dom';
+import profileImage from '../../img/placeholder.png'; // Import the profile image
 
 const AppBarComponent = () => {
   return (
-    <AppBar position="static">
+    <AppBar position="static" sx={{ backgroundColor: 'white' }}>
       <Toolbar>
-        <IconButton edge="start" color="inherit" aria-label="menu">
-          <Menu />
-        </IconButton>
-        <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-          WalkSeattle
-        </Typography>
+        <Logo style={{ height: '40px', width: 'auto', marginRight: '16px' }} />
+        <Link to="/profile" style={{ marginLeft: 'auto' }}>
+          <Avatar
+            alt="User Profile"
+            src={profileImage}
+            sx={{ width: 40, height: 40 }}
+          />
+        </Link>
       </Toolbar>
     </AppBar>
   );
